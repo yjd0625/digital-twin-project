a5f778d290aca5615c1ad27476688dd1171362bf
yjd0625 <303182430@qq.com>
2026-07-24 08:55:30 +0800

    feat: multi-select (Ctrl+click), optimized DXF (LineSegments), click plane, Delete for labels

--- frontend/src/scene.js
+++ frontend/src/scene.js
@@ -52,7 +52,7 @@ export function createScene(container) {
   scene.add(backLight);
 
   // --- 地面网格（帮助判断空间位置） ---
-  const grid = new THREE.GridHelper(10, 20, 0x88aaff, 0x335588);
+  const grid = new THREE.GridHelper(30, 20, 0x88aaff, 0x335588);
   grid.position.y = 0;
   scene.add(grid);
 
